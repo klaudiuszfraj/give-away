e5401363b2d4038ff5882f5d711a39432e083656
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-09-07 18:23:04 +0200

    add stats 3 columns

--- src/components/Home/Stats/Starts.jsx
+++ src/components/Home/Stats/Starts.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
+import style from './Stats.module.scss'
+
 function Starts() {
- return (
-  <div style={{height: 3000}}>
-   <h1>Starts</h1>
-  </div>
- );
+    const [givenBags, setGivenBags] = useState('10');
+    const [organizations, setOrganizations] = useState('10');
+    const [collection, setCollection] = useState('10');
+
+
+    return (
+        <section className={style.stats}>
+            <div>
+                <h1>{givenBags}</h1>
+                <h2>Oddanych worków</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+            </div>
+            <div>
+                <h1>{organizations}</h1>
+                <h2>Oddanych worków</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+            </div>
+            <div>
+                <h1>{collection}</h1>
+                <h2>Oddanych worków</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+            </div>
+
+
+        </section>
+    );
 }
+
 export default Starts;

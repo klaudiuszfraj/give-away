77762255dbe4111ae53fcdca6c1ed1bd16858900
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-09-07 17:45:02 +0200

    add Hero style

--- src/components/Home/Hero/Hero.jsx
+++ src/components/Home/Hero/Hero.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import style from './Hero.module.scss';
+import HeroImg from '../../../assets/Home-Hero-Image.jpg';
+import { ReactComponent as Decoration } from '../../../assets/Decoration.svg';
+
 function Hero() {
- return (
-  <div>
-   <h1>Hero</h1>
-  </div>
- );
+    return (
+        <section className={style.hero}>
+            <div className={style.hero__img}/>
+            <div className={style.hero__info}>
+            <h1>Zacznij pomagać!<br/>Oddaj niechciane rzeczy w zaufane ręce</h1>
+            <Decoration/>
+            <div className={style.hero__buttons}>
+                <button>Oddaj rzeczy</button>
+                <button>Zorganizuj zbiórję</button>
+            </div>
+            </div>
+        </section>
+    );
 }
+
 export default Hero;

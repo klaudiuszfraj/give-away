cd3d8edd0e18d8b99dd2a6502f7754a7d068f973
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-09-07 14:28:24 +0200

    create styles for Home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Header, Hero, Stats, FourSteps, About, WhoWeHelp, Contact, Footer } from "../index";
-
+import style from './Home.module.scss'
 function Home() {
  return (
-  <div>
+  <div className={style.home__container}>
       <Header/>
       <Hero/>
       <Stats/>

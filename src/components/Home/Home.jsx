d18f917d7644014b9ddd65aeb7ce9cf1ab90cc2b
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-09-07 12:55:46 +0200

    h1 home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 function Home() {
  return (
   <div>
-   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita fugit id obcaecati! Deleniti dicta ipsa porro vitae! Excepturi fugit illum incidunt libero molestiae nisi, numquam perspiciatis porro repellat similique unde.</p>
+      <h1>Home</h1>
   </div>
  );
 }

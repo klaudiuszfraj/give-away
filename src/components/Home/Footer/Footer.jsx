5245982ed08e7ea6a067f39df1ffb3b4ec35a08a
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-09-07 13:50:04 +0200

    create Footer

---
+++ src/components/Home/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ReactComponent as Instagram } from '../../../assets/Instagram.svg';
+import { ReactComponent as Facebook } from '../../../assets/Facebook.svg';
+//todo:: import facebook icon
+
+import style from './Footer.module.scss';
+
+function Footer() {
+ return (
+  <footer className={style.footer}>
+   <p>Copyright by Coders Lab</p>
+      <div className={style.icons}>
+        <Instagram/>
+        <Facebook/>
+      </div>
+  </footer>
+ );
+}
+export default Footer;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Footer } from "../index";
+
 function Home() {
  return (
   <div>
       <h1>Home</h1>
+      <Footer/>
   </div>
  );
 }

6b1737e152afa5cdbd67da60cdd8f3cfd5c3b547
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-09-09 13:42:34 +0200

    create empty ListItem

---
+++ src/components/Home/WhoWeHelp/ListItem/ListItem.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import style from './ListItem.module.scss';
+
+function ListItem() {
+
+
+ return (
+  <div className={style.ListItem}>
+
+  </div>
+ );
+}
+export default ListItem;

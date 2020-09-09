768820d25cb3f6e1eab4099db8b7403a88fe8d0c
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-09-09 11:39:24 +0200

    add about component

--- src/components/Home/About/About.jsx
+++ src/components/Home/About/About.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import style from './About.module.scss';
+import { ReactComponent as Decoration } from "../../../assets/Decoration.svg";
+// import { ReactComponent as Signature } from "../../../assets/Signature.svg";
+//todo::  nie ładuje sie w pełni
+import signature from "../../../assets/Signature.svg";
 function About() {
+
+
  return (
-  <div>
-   <h1>About</h1>
-  </div>
+  <section className={style.about} id={'aboutUs'}>
+      <div className={style.about__info}>
+          <div className={style.about__info__content}>
+              <h1>O nas</h1>
+              <Decoration/>
+              <p>Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
+              {/*<Signature/>*/}
+              <img src={signature} alt=""/>
+          </div>
+      </div>
+      <div className={style.about__img}>
+      </div>
+  </section>
  );
 }
 export default About;

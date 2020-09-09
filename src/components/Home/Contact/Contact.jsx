194a2a3a00a03b1f70530cd5589904b69d544bc9
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-09-09 15:36:30 +0200

    create structire Contact

--- src/components/Home/Contact/Contact.jsx
+++ src/components/Home/Contact/Contact.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import style from './Contact.module.scss';
+import { ReactComponent as Decoration } from '../../../assets/Decoration.svg'
+
 function Contact() {
- return (
-  <div className={'contact'}>
-    <h1>Contact</h1>
-  </div>
- );
+
+
+    return (
+        <section className={style.Contact} id={'contact'}>
+            <div className={style.Contact__img}/>
+            <div className={style.Contact__container}>
+                <div className={style.Contact__content}>
+                    <h1>Skontaktuj się z nami</h1>
+                    <Decoration/>
+                    <form action="">
+                        <label htmlFor="name">Wpisz swoje imię</label>
+                        <input type="text" id={'name'}/>
+
+                        <label htmlFor="email">Wpisz swój email</label>
+                        <input type="text" id={'email'}/>
+
+                        <label htmlFor="message">Wpisz swoją wiadomość</label>
+                        <textarea name="message" id="message" cols="30" rows="10"></textarea>
+
+                        <button>Wyślij</button>
+                    </form>
+                </div>
+            </div>
+        </section>
+    );
 }
+
 export default Contact;

ab73362c6f3ae6a3e6a467c422a8fff2cbfecc1d
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-09-10 12:15:13 +0200

    fetch array

--- src/API/index.js
+++ src/API/index.js
@@ -4,9 +4,9 @@ const API_URL = 'http://localhost:3000/db';
 
 export const fetchItemList = async ()=>{
     try{
-        const {data} = await axios.get(API_URL);
-        console.log(data);
-        return data;
+        const {data: { fundations }} = await axios.get(API_URL);
+        console.log(fundations);
+        return fundations;
     }
     catch (error){
         console.log(error);

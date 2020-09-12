1a4d147175c453817d5457af60e24e33e218590a
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-09-12 18:55:41 +0200

    create basic structure

---
+++ src/components/Logout/Logout.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Header} from "../index";
+import style from './Logout.module.scss'
+import {ReactComponent as Decoration} from "../../assets/Decoration.svg";
+import {Link} from "react-router-dom";
+
+
+function Logout() {
+    return (
+        <>
+            <Header/>
+            <main className={style.Logout}>
+                <h1>Wylogowanie nastapiło pomyślnie!</h1>
+                <Decoration/>
+                <Link to={'/'}>Strona główna</Link>
+            </main>
+        </>
+    );
+}
+
+export default Logout;

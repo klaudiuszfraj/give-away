20c1fa3e1b76aa4cb1c2a2cee2ac6a5cf70eea3e
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-09-12 09:30:06 +0200

    correct typo

--- src/API/index.js
+++ src/API/index.js
@@ -2,11 +2,10 @@ import axios from 'axios';
 
 const API_URL = 'http://localhost:3000/db';
 
-export const fetchItemList = async ()=>{
+export const fetchFoundationsList = async ()=>{
     try{
-        const {data: { fundations }} = await axios.get(API_URL);
-        console.log(fundations);
-        return fundations;
+        const {data: { foundations }} = await axios.get(API_URL);
+        return foundations;
     }
     catch (error){
         console.log(error);

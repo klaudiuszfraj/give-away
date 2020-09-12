c862e307d8df9a0133d68859200ab717fb4a5db4
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-09-12 18:17:54 +0200

    basic content

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,13 +1,37 @@
 import React, {useState} from 'react';
-
+import {Header} from "../index";
+import {ReactComponent as Decoration} from "../../assets/Decoration.svg";
+import style from './Login.module.scss';
+import {Link} from "react-router-dom";
 
 function Login() {
 
 
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
+        <>
+            <Header/>
+            <main className={style.Login}>
+                <div>
+                    <h1>Zaloguj się</h1>
+                    <Decoration/>
+                    <div>
+                        <form action="">
+                            <div className={style.Login__inputs}>
+                                <label htmlFor="email">Email</label>
+                                <input type="text" id={'email'}/>
+                                <label htmlFor="password">Hasło</label>
+                                <input type="password" id={'password'}/>
+                            </div>
+                            <div className={style.Login__btns}>
+                                <Link to={'/rejestracja'}>Załóż konto</Link>
+                                <button>Zaloguj się</button>
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+            </main>
+        </>
     );
 }
 

490c28a8e93b540c21b9b61d05935aad6b8174c8
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-09-13 11:14:40 +0200

    create validation

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Header} from "../index";
 import style from './Registration.module.scss'
 import {ReactComponent as Decoration} from "../../assets/Decoration.svg";
@@ -7,71 +7,95 @@ import {Link} from "react-router-dom";
 
 function Registration() {
 
- const [email, setEmail] = useState('')
- const [password, setPassword] = useState('')
- const [password2, setPassword2] = useState('')
- const [errors, setErrors] = useState({
-  email: false,
-  password: false,
-  password2: false,
-  success: false
- })
- const handleSubmit = (e)=>{
-  e.preventDefault();
-  function validateEmail(email) {
-   if (email === ''){
-    return false
-   }
-   const re = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
-   return re.test(String(email).toLowerCase());
-  }
-  function validatePassword(password){
-   if (password === '' || password.length < 6){
-    return false
-   }
-   const re = /^[A-Za-z]*$/;
-   return re.test(String(password))
-  }
-  setErrors({
-   email: !validateEmail(email),
-   password: !validatePassword(password),
-   password2: !validatePassword(password2),
-   success: false
-  })
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [password2, setPassword2] = useState('')
+    const [errors, setErrors] = useState({
+        email: false,
+        password: false,
+        password2: false,
+        success: false
+    })
+    const [user, setUser] = useState({})
+
+    useEffect(()=>{
+        if (errors.success) {
+            setUser({
+                email: email,
+                password: password
+            })
+        }
+    },[errors.success])
 
 
- }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        function validateEmail(email) {
+            if (email === '') {
+                return false
+            }
+            const re = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+            return re.test(String(email).toLowerCase());
+        }
+
+        function validatePassword(password) {
+            if (password === '' || password.length < 6) {
+                return false
+            }
+            const re = /^[A-Za-z]*$/;
+            return re.test(String(password))
+        }
+
+        function validatePassword2(password2) {
+            if (password2 === '' || password2 !== password) {
+                return false
+            }
+            const re = /^[A-Za-z]*$/;
+            return re.test(String(password2))
+        }
+
+        setErrors({
+            email: !validateEmail(email),
+            password: !validatePassword(password),
+            password2: !validatePassword2(password2),
+            success: validateEmail(email) && validatePassword(password) && validatePassword2(password2)
+        })
+    }
 
 
-
- return (
+    return (
         <>
-         <Header/>
-         <main className={style.Registration}>
-          <div className={style.Registration__container}>
-           <h1>Załóż konto</h1>
-           <Decoration/>
-           <form action="" onSubmit={e=>handleSubmit(e)}>
-            <div className={style.Registration__inputs}>
-             <label htmlFor="email">Email</label>
-             <input type="text" id={'email'} value={email} onChange={e=>setEmail(e.target.value)}/>
-             <small className={classNames({'error':errors.email})}>Podany email jest niepoprawny</small>
+            <Header/>
+            <main className={style.Registration}>
+                <div className={style.Registration__container}>
+                    <h1>Załóż konto</h1>
+                    <Decoration/>
+                    <form action="" onSubmit={e => handleSubmit(e)}>
+                        <div className={style.Registration__inputs}>
+                            <label htmlFor="email">Email</label>
+                            <input type="text" id={'email'} value={email} onChange={e => setEmail(e.target.value)}/>
+                            <small className={classNames({'error': errors.email})}>Podany email jest niepoprawny</small>
 
-             <label htmlFor="password">Hasło</label>
-             <input type="password" id={'password'} value={password} onChange={e=>setPassword(e.target.value)}/>
-             <small className={classNames({'error': errors.password})}>Podane hasło jest niepoprawne</small>
+                            <label htmlFor="password">Hasło</label>
+                            <input type="password" id={'password'} value={password}
+                                   onChange={e => setPassword(e.target.value)}/>
+                            <small className={classNames({'error': errors.password})}>Podane hasło jest
+                                niepoprawne</small>
 
-             <label htmlFor="password2">Powtórz hasło</label>
-             <input type="password" id={'password2'} value={password2} onChange={e=>setPassword2(e.target.value)}/>
-             <small className={classNames({'error': errors.password2})}>Podane hasło jest niepoprawne</small>
-            </div>
-            <div className={style.Registration__btns}>
-             <Link to={'/rejestracja'}>Załóż konto</Link>
-             <button>Zaloguj się</button>
-            </div>
-           </form>
-          </div>
-         </main>
+                            <label htmlFor="password2">Powtórz hasło</label>
+                            <input type="password" id={'password2'} value={password2}
+                                   onChange={e => setPassword2(e.target.value)}/>
+                            <small className={classNames({'error': errors.password2})}>Podane hasło jest
+                                niepoprawne</small>
+                        </div>
+                        <div className={style.Registration__btns}>
+                            <Link to={'/logowanie'}>Zaloguj się</Link>
+                            <button>Załóż konto</button>
+                        </div>
+                    </form>
+                </div>
+            </main>
         </>
     );
 }

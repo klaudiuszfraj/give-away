8277271a9374e92a9f2e3b8c67ba18aed1843a83
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-09-14 17:28:30 +0200

    switch Link names

--- src/components/Home/Header/Header.jsx
+++ src/components/Home/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Link } from 'react-scroll';
-import { Link as UserLinks } from 'react-router-dom';
+import { Link as ScrollLink} from 'react-scroll';
+import { Link } from 'react-router-dom';
 import style from './Header.module.scss';
 import classNames from 'classnames';
 import {useSelector} from "react-redux";
@@ -15,22 +15,22 @@ function Header() {
                 {userLoggedIn.isLogged ?
                     <>
                    <p>Cześć {userLoggedIn.email}</p>
-                   <UserLinks to={'/oddaj-rzeczy'}>Oddaj rzeczy</UserLinks>
-                   <UserLinks to={'/wylogowano'}>Wyloguj</UserLinks>
+                   <Link to={'/oddaj-rzeczy'}>Oddaj rzeczy</Link>
+                   <Link to={'/wylogowano'}>Wyloguj</Link>
                    </>
                 :
                     <>
-                    <UserLinks to={'/logowanie'}>Zaloguj</UserLinks>
-                    <UserLinks to={'/rejestracja'}>Załóż konto</UserLinks>
+                    <Link to={'/logowanie'}>Zaloguj</Link>
+                    <Link to={'/rejestracja'}>Załóż konto</Link>
                     </>
                 }
             </div>
             <nav>
-                <Link to={'start'} smooth={true} duration={1000}>Start</Link>
-                <Link to={'FourSteps'} smooth={true} duration={1000}>O co chodzi?</Link>
-                <Link to={'aboutUs'} smooth={true} duration={1000}>O nas</Link>
-                <Link to={'foundation'} smooth={true} duration={1000}>Fundcja i organizacje</Link>
-                <Link to={'contact'} smooth={true} duration={1000}>Kontakt</Link>
+                <ScrollLink to={'start'} smooth={true} duration={1000}>Start</ScrollLink>
+                <ScrollLink to={'FourSteps'} smooth={true} duration={1000}>O co chodzi?</ScrollLink>
+                <ScrollLink to={'aboutUs'} smooth={true} duration={1000}>O nas</ScrollLink>
+                <ScrollLink to={'foundation'} smooth={true} duration={1000}>Fundcja i organizacje</ScrollLink>
+                <ScrollLink to={'contact'} smooth={true} duration={1000}>Kontakt</ScrollLink>
             </nav>
         </header>
     );

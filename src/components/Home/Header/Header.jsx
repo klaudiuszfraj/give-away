6aa7135880d261e2e9e36bf2ba9fae3254a940f5
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-09-14 15:16:21 +0200

    add global state and set display after logIn

--- src/components/Home/Header/Header.jsx
+++ src/components/Home/Header/Header.jsx
@@ -2,13 +2,29 @@ import React from 'react';
 import { Link } from 'react-scroll';
 import { Link as UserLinks } from 'react-router-dom';
 import style from './Header.module.scss';
+import classNames from 'classnames';
+import {useSelector, useDispatch} from "react-redux";
+// import logOut
+
 
 function Header() {
+    const userLoggedIn = useSelector(state=>state.isLogged)
+    console.log('user w header',userLoggedIn);
     return (
         <header className={style.header}>
-            <div>
-                <UserLinks to={'/logowanie'}>Zaloguj</UserLinks>
-                <UserLinks to={'/rejestracja'}>Załóż konto</UserLinks>
+            <div className={classNames({'loggedIn': userLoggedIn.isLogged})}>
+                {userLoggedIn.isLogged ?
+                    <>
+                   <p>Cześć {userLoggedIn.email}</p>
+                   <UserLinks to={'/oddaj-rzeczy'}>Oddaj rzeczy</UserLinks>
+                   <UserLinks to={'/wylogowano'}>Wyloguj</UserLinks>
+                   </>
+                :
+                    <>
+                    <UserLinks to={'/logowanie'}>Zaloguj</UserLinks>
+                    <UserLinks to={'/rejestracja'}>Załóż konto</UserLinks>
+                    </>
+                }
             </div>
             <nav>
                 <Link to={'start'} smooth={true} duration={1000}>Start</Link>

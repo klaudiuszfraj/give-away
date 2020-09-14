2430832b71dd1c33ee457582a9c94405a178225e
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-09-14 11:02:27 +0200

    set up store

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from "redux";
+import allReducers from './reducers/index';
+import {Provider} from 'react-redux';
+
+const store = createStore(
+    allReducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import {combineReducers} from "redux";
+
+import isLogged from "./isLogged";
+
+const allReducers = combineReducers({
+    isLogged
+});
+
+export default allReducers

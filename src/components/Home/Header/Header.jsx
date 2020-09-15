f857e6d54102f76a7c13c9558ecec703caf6b417
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-09-15 14:25:27 +0200

    add condition render in nav

--- src/components/Home/Header/Header.jsx
+++ src/components/Home/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link as ScrollLink} from 'react-scroll';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import style from './Header.module.scss';
 import classNames from 'classnames';
 import {useSelector} from "react-redux";
@@ -9,6 +9,8 @@ import {useSelector} from "react-redux";
 function Header() {
     const userLoggedIn = useSelector(state=>state.isLogged)
     console.log('user w header',userLoggedIn);
+    const currentLocation = useLocation();
+
     return (
         <header className={style.header}>
             <div className={classNames({'loggedIn': userLoggedIn.isLogged})}>
@@ -26,11 +28,24 @@ function Header() {
                 }
             </div>
             <nav>
-                <ScrollLink to={'start'} smooth={true} duration={1000}>Start</ScrollLink>
-                <ScrollLink to={'FourSteps'} smooth={true} duration={1000}>O co chodzi?</ScrollLink>
-                <ScrollLink to={'aboutUs'} smooth={true} duration={1000}>O nas</ScrollLink>
-                <ScrollLink to={'foundation'} smooth={true} duration={1000}>Fundcja i organizacje</ScrollLink>
-                <ScrollLink to={'contact'} smooth={true} duration={1000}>Kontakt</ScrollLink>
+                {currentLocation.pathname === '/'
+                    ?
+                    <>
+                        <ScrollLink to={'start'} spy={true} smooth={true} duration={1000}>Start</ScrollLink>
+                        <ScrollLink to={'FourSteps'} smooth={true} duration={1000}>O co chodzi?</ScrollLink>
+                        <ScrollLink to={'aboutUs'} smooth={true} duration={1000}>O nas</ScrollLink>
+                        <ScrollLink to={'foundation'} smooth={true} duration={1000}>Fundcja i organizacje</ScrollLink>
+                        <ScrollLink to={'contact'} hashSpy={true} smooth={true} duration={1000}>Kontakt</ScrollLink>
+                    </>
+                    :
+                    <>
+                        <Link to={'/#start'}>Start</Link>
+                        <Link to={'/#FourSteps'}>O co chodzi?</Link>
+                        <Link to={'/#aboutUs'}>O nas</Link>
+                        <Link to={'/#foundation'}>Fundcja i organizacje</Link>
+                        <Link to={'/#contact'}>Kontakt</Link>
+                    </>
+                }
             </nav>
         </header>
     );

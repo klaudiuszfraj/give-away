ec1bcb4e13531487e1642a45426f1a084d81fb97
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-09-16 11:44:05 +0200

    comment useDispatch

--- src/components/Logout/Logout.jsx
+++ src/components/Logout/Logout.jsx
@@ -3,12 +3,15 @@ import {Header} from "../index";
 import style from './Logout.module.scss'
 import {ReactComponent as Decoration} from "../../assets/Decoration.svg";
 import {Link} from "react-router-dom";
-import {useDispatch} from "react-redux";
-import {logOut} from "../../actions";
+// import {useDispatch} from "react-redux";
+// import {logOut} from "../../actions";
 
+// Attempted import error: 'useDispatch' is not exported from 'react-redux'.
+
+//todo:: wylogowywaqnie
 function Logout() {
-    const dispatch = useDispatch()
-    dispatch(logOut());
+    // const dispatch = useDispatch()
+    // dispatch(logOut());
     return (
         <>
             <Header/>

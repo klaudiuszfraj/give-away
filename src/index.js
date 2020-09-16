687604c299fabc8bb64649041d089942910f3c78
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-09-16 13:02:50 +0200

    add packages to comunicate with firebase

--- src/index.js
+++ src/index.js
@@ -7,15 +7,23 @@ import {createStore, applyMiddleware, compose} from "redux";
 import allReducers from './reducers/index';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
+import {reduxFirestore, getFirestore} from "redux-firestore";
+import {reactReduxFirebase, getFirebase} from "react-redux-firebase";
+import fbConfig from './config/fbConfig';
 
-// const store = createStore(
-//     allReducers,
-//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-// )
+
 const store = createStore(
     allReducers,
-    compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-)
+    compose(
+        applyMiddleware(thunk.withExtraArgument({
+            getFirebase,
+            getFirestore
+        })),
+        reduxFirestore(fbConfig),
+        reactReduxFirebase(fbConfig),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+);
 
 
 ReactDOM.render(

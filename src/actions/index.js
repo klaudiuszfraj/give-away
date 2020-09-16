b9ea56211f25c46ea917f42630756c8149fffcc7
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-09-16 13:01:21 +0200

    add registerUser action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 export const logIn = (user)=>{
-    return (dispatch, getState) =>{
+    return (dispatch, getState, { getFirebase, getFirestore }) =>{
         //async call to database
         dispatch({
             type: 'LOGIN',
@@ -11,5 +11,18 @@ export const logIn = (user)=>{
 export const logOut = ()=>{
     return {
         type: 'LOGOUT',
+    }
+}
+
+export const registerUser = (user)=>{
+    return (dispatch, getState, { getFirebase, getFirestore }) => {
+        // async function to firestore
+        const firestore = getFirestore();
+        firestore.collection("users").add({...user}).then(()=>{
+            dispatch({type: 'REGISTER', payload: user});
+        }).catch(error=>{
+            dispatch({type: 'REGISTER_ERROR', payload: error});
+        })
+
     }
 }

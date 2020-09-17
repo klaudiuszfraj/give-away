3e80e3bd66e67e9c0622433e85ffc6b57afdb931
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-09-17 10:34:01 +0200

    change login action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,42 @@
-export const logIn = (user)=>{
-    return (dispatch, getState, { getFirebase, getFirestore }) =>{
-        //async call to database
-        dispatch({
-            type: 'LOGIN',
-            payload: user
+// export const LOGIN_PENDING = "[user] - Start loging";
+// export const LOGIN_SUCCESS = "[user] - Logged user"
+
+// export const logIn = ({ email, password })=>{
+//     return (dispatch, getState, { getFirebase, getFirestore }) =>{
+//         //async call to database
+//         dispatch({
+//             type: LOGIN_PENDING
+//         })
+//         getFirebase().auth().signInWithEmailAndPassword(email, password)
+//             .then(user => {
+//                 dispatch({
+//                     type: LOGIN_SUCCESS,
+//                     payload: user
+//                 })
+//             })
+//             .catch(function(error) {
+//                 console.log(error)
+//         });
+//
+//     };
+// };
+
+export const signIn = user => {
+    return (dispatch, getState, {getFirebase}) => {
+        const firebase = getFirebase();
+
+        firebase.auth().signInWithEmailAndPassword(
+            user.email,
+            user.password
+        ).then(()=>{
+            dispatch({type: 'LOGIN_SUCCESS'})
+        }).catch(error => {
+            dispatch({type: 'LOGIN_ERROR', payload: error});
         })
-    };
-};
+    }
+}
+
+
 
 export const logOut = ()=>{
     return {
@@ -14,15 +44,21 @@ export const logOut = ()=>{
     }
 }
 
-export const registerUser = (user)=>{
+export const registerUser = ({email, password})=>{
     return (dispatch, getState, { getFirebase, getFirestore }) => {
         // async function to firestore
         const firestore = getFirestore();
-        firestore.collection("users").add({...user}).then(()=>{
-            dispatch({type: 'REGISTER', payload: user});
-        }).catch(error=>{
-            dispatch({type: 'REGISTER_ERROR', payload: error});
-        })
+        // firestore.collection("users").add({...user}).then(()=>{
+        //     dispatch({type: 'REGISTER', payload: user});
+        // }).catch(error=>{
+        //     dispatch({type: 'REGISTER_ERROR', payload: error});
+        // })
+
+        getFirebase().auth().createUserWithEmailAndPassword(email, password)
+            .then(user => console.log(user))
+            .catch(function(error) {
+            console.log(error)
+        });
 
     }
 }
--- src/reducers/isLogged.js
+++ src/reducers/isLogged.js
@@ -1,11 +1,19 @@
 const isLogged = (state = {
-    email: '',
-    password: '',
-    isLogged: false
+    authError: null
 }, action) => {
     switch (action.type) {
-        case 'LOGIN':
-            return action.payload;
+        case 'LOGIN_SUCCESS':
+            console.log('succesd');
+            return {
+                ...state,
+                authError: null
+            };
+        case 'LOGIN_ERROR':
+            console.log('errr');
+            return {
+                ...state,
+                authError: 'Login failed'
+            }
         case 'LOGOUT':
             return {
                 ...state,

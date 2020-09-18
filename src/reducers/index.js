4eaa8c8b17bfa9d6a24991e6d0a7f5c9179271e7
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-09-18 11:01:48 +0200

    remove register.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,10 @@ import {firestoreReducer} from "redux-firestore";
 import {firebaseReducer} from "react-redux-firebase";
 
 import authentication from "./authentication";
-import register from "./register";
 import foundationList from "./foundationsList";
 
 const allReducers = combineReducers({
     isLogged: authentication,
-    register,
     foundationList,
     firestore: firestoreReducer,
     firebase: firebaseReducer

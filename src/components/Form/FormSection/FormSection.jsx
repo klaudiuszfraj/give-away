ec67c7c2679c497db35653c948ff13ae5dc39ea3
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-09-27 11:59:57 +0200

    add first step content with basic style

--- src/components/Form/FormSection/FormSection.jsx
+++ src/components/Form/FormSection/FormSection.jsx
@@ -2,10 +2,37 @@ import React from 'react';
 import style from './FormSection.module.scss';
 
 function FormSection() {
- return (
-  <div>
-   <h1>form</h1>
-  </div>
- );
+    return (
+        <section className={style.FormSection}>
+            <div className={style.FormSection__container}>
+                <h4>Krok 1/4</h4>
+                <h2>Zaznacz co chcesz oddać:</h2>
+                <form action="#">
+                    <div>
+                        <input type="checkbox" name={'clothesToUse'}/>
+                        <label htmlFor="clothesToUse">ubrania, które nadają się do ponownego użycia</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" name={'clothesToTrash'}/>
+                        <label htmlFor="clothesToTrash">ubrania, do wyrzucenia</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" name={'toys'}/>
+                        <label htmlFor="toys">zabawki</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" name={'books'}/>
+                        <label htmlFor="books">książki</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" name={'different'}/>
+                        <label htmlFor="different">inne</label>
+                    </div>
+                </form>
+                <button>Dalej</button>
+            </div>
+        </section>
+    );
 }
+
 export default FormSection;

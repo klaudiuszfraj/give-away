b9eb193cb60e6741cfdcda4f0833481ff8937c46
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-09-27 10:34:04 +0200

    migrate FormHero section to FormHero component

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,37 +1,10 @@
 import React from 'react';
 import {Contact, Footer, FormHero} from "../index";
 import style from './Form.module.scss'
-import {ReactComponent as Decoration} from "../../assets/Decoration.svg";
 
 function Form() {
     return (
         <>
-            <section className={style.Form__hero}>
-                <div className={style.Form__hero__img}/>
-                <div className={style.Form__hero__info}>
-                    <h1>Oddaj rzeczy, których już nie chcesz <br/> POTRZEBUJĄCYM</h1>
-                    <Decoration/>
-                    <p>Wystarczą 4 proste kroki</p>
-                    <div className={style.hero__boxes}>
-                        <div>
-                            <p>1</p>
-                            <p>Wybierz rzeczy</p>
-                        </div>
-                        <div>
-                            <p>2</p>
-                            <p>Spakuj je w worki</p>
-                        </div>
-                        <div>
-                            <p>3</p>
-                            <p>Wybierz fundacje</p>
-                        </div>
-                        <div>
-                            <p>4</p>
-                            <p>Zamów kuriera</p>
-                        </div>
-                    </div>
-                </div>
-            </section>
             <FormHero/>
             <section className={style.Form__important}>
                 <h2>Ważne!</h2>
--- src/components/Form/FormHero/FormHero.jsx
+++ src/components/Form/FormHero/FormHero.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
 import style from './FormHero.module.scss'
+import {ReactComponent as Decoration} from "../../../assets/Decoration.svg";
 
 function FormHero() {
- return (
-  <div>
-   <h1 >FormHero</h1>
-  </div>
- );
+    return (
+        <section className={style.Form__hero}>
+            <div className={style.Form__hero__img}/>
+            <div className={style.Form__hero__info}>
+                <h1>Oddaj rzeczy, których już nie chcesz <br/> POTRZEBUJĄCYM</h1>
+                <Decoration/>
+                <p>Wystarczą 4 proste kroki</p>
+                <div className={style.hero__boxes}>
+                    <div>
+                        <p>1</p>
+                        <p>Wybierz rzeczy</p>
+                    </div>
+                    <div>
+                        <p>2</p>
+                        <p>Spakuj je w worki</p>
+                    </div>
+                    <div>
+                        <p>3</p>
+                        <p>Wybierz fundacje</p>
+                    </div>
+                    <div>
+                        <p>4</p>
+                        <p>Zamów kuriera</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
+
 export default FormHero;

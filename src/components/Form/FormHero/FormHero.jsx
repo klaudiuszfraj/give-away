1e54163ba329931ed2b2393437e153cfb707fdc8
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-09-27 10:38:47 +0200

    use map to render steps

--- src/components/Form/FormHero/FormHero.jsx
+++ src/components/Form/FormHero/FormHero.jsx
@@ -3,6 +3,9 @@ import style from './FormHero.module.scss'
 import {ReactComponent as Decoration} from "../../../assets/Decoration.svg";
 
 function FormHero() {
+
+    const steps = ['Wybierz rzeczy', 'Spakuj je w worki', 'Wybierz fundacje', 'Zamów kuriera'];
+
     return (
         <section className={style.Form__hero}>
             <div className={style.Form__hero__img}/>
@@ -10,23 +13,14 @@ function FormHero() {
                 <h1>Oddaj rzeczy, których już nie chcesz <br/> POTRZEBUJĄCYM</h1>
                 <Decoration/>
                 <p>Wystarczą 4 proste kroki</p>
+
                 <div className={style.hero__boxes}>
-                    <div>
-                        <p>1</p>
-                        <p>Wybierz rzeczy</p>
-                    </div>
-                    <div>
-                        <p>2</p>
-                        <p>Spakuj je w worki</p>
-                    </div>
-                    <div>
-                        <p>3</p>
-                        <p>Wybierz fundacje</p>
-                    </div>
-                    <div>
-                        <p>4</p>
-                        <p>Zamów kuriera</p>
-                    </div>
+                    {steps.map((step, index) => (
+                        <div>
+                            <p>{index + 1}</p>
+                            <p>{step}</p>
+                        </div>
+                    ))}
                 </div>
             </div>
         </section>

643cdcb6d245e8c6219cf00cc610943e07bd17b4
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-09-28 12:56:26 +0200

    add form reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,10 +4,12 @@ import {firebaseReducer} from "react-redux-firebase";
 
 import authentication from "./authentication";
 import foundationList from "./foundationsList";
+import form from "./form";
 
 const allReducers = combineReducers({
     isLogged: authentication,
     foundationList,
+    form,
     firestore: firestoreReducer,
     firebase: firebaseReducer
 });

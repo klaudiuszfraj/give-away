fb2bd2d42404fbde3bc68d86c9e487039a4dfd94
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-09-28 10:31:55 +0200

    add input info to state

--- src/components/Form/FormSection/FormSection.jsx
+++ src/components/Form/FormSection/FormSection.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import style from './FormSection.module.scss';
 
 function FormSection() {
+
+    const [step1, setStep1] = useState({
+        clothesToUse: false,
+        clothesToTrash: false,
+        toys: false,
+        books: false,
+        different: false
+    })
+
+    const handleInputs = (event)=> {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        setStep1(prevState => ({
+            ...prevState,
+            [name]: value
+        }))
+    }
+
     return (
         <section className={style.FormSection}>
             <div className={style.FormSection__container}>
@@ -9,23 +29,23 @@ function FormSection() {
                 <h2>Zaznacz co chcesz oddać:</h2>
                 <form action="#">
                     <div>
-                        <input type="checkbox" name={'clothesToUse'}/>
+                        <input type="checkbox" name={'clothesToUse'} id={'clothesToUse'} value={step1.clothesToUse} onClick={handleInputs}/>
                         <label htmlFor="clothesToUse">ubrania, które nadają się do ponownego użycia</label>
                     </div>
                     <div>
-                        <input type="checkbox" name={'clothesToTrash'}/>
+                        <input type="checkbox" name={'clothesToTrash'} id={'clothesToTrash'} value={step1.clothesToTrash} onClick={handleInputs}/>
                         <label htmlFor="clothesToTrash">ubrania, do wyrzucenia</label>
                     </div>
                     <div>
-                        <input type="checkbox" name={'toys'}/>
+                        <input type="checkbox" name={'toys'} id={'toys'} value={step1.toys} onClick={handleInputs}/>
                         <label htmlFor="toys">zabawki</label>
                     </div>
                     <div>
-                        <input type="checkbox" name={'books'}/>
+                        <input type="checkbox" name={'books'} id={'books'} value={step1.books} onClick={handleInputs}/>
                         <label htmlFor="books">książki</label>
                     </div>
                     <div>
-                        <input type="checkbox" name={'different'}/>
+                        <input type="checkbox" name={'different'} id={'different'} value={step1.different} onClick={handleInputs}/>
                         <label htmlFor="different">inne</label>
                     </div>
                 </form>

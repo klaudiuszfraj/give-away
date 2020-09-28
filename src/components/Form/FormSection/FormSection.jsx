ec2bf0e332099e0058d43cf858e5ed2ffe00813d
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-09-28 13:59:07 +0200

    export step1 to its own component

--- src/components/Form/FormSection/FormSection.jsx
+++ src/components/Form/FormSection/FormSection.jsx
@@ -1,35 +1,12 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import style from './FormSection.module.scss';
 import {connect} from "react-redux";
-import {submitStep, updateStep} from "../../../actions";
+import {submitStep} from "../../../actions";
+import Step1 from "./Steps/Step1";
 
-function FormSection({form: {clothesToUse, clothesToTrash, toys, books, different}, submitStep, updateStep}) {
+function FormSection({form, submitStep}) {
     const [currentStep, setCurrentStep] = useState(1)
-    // const [step1, setStep1] = useState({
-    //     clothesToUse: false,
-    //     clothesToTrash: false,
-    //     toys: false,
-    //     books: false,
-    //     different: false
-    // })
 
-
-    const handleInputs = (event) => {
-        const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
-        const name = target.name;
-
-        updateStep({
-            [name]: value
-        })
-
-        // setStep1(prevState => ({
-        //     ...prevState,
-        //     [name]: value
-        // }))
-
-
-    };
     const handleSubmit = (e) => {
         e.preventDefault();
     }
@@ -38,33 +15,8 @@ function FormSection({form: {clothesToUse, clothesToTrash, toys, books, differen
         <section className={style.FormSection}>
             <div className={style.FormSection__container}>
                 <h4>Krok {currentStep}/4</h4>
-                <h2>Zaznacz co chcesz oddać:</h2>
                 <form action="#">
-                    <div>
-                        <input type="checkbox" name={'clothesToUse'} id={'clothesToUse'} value={clothesToUse}
-                               checked={clothesToUse} onClick={handleInputs}/>
-                        <label htmlFor="clothesToUse">ubrania, które nadają się do ponownego użycia</label>
-                    </div>
-                    <div>
-                        <input type="checkbox" name={'clothesToTrash'} id={'clothesToTrash'} value={clothesToTrash}
-                               checked={clothesToTrash} onClick={handleInputs}/>
-                        <label htmlFor="clothesToTrash">ubrania, do wyrzucenia</label>
-                    </div>
-                    <div>
-                        <input type="checkbox" name={'toys'} id={'toys'} value={toys} checked={toys}
-                               onClick={handleInputs}/>
-                        <label htmlFor="toys">zabawki</label>
-                    </div>
-                    <div>
-                        <input type="checkbox" name={'books'} id={'books'} value={books} checked={books}
-                               onClick={handleInputs}/>
-                        <label htmlFor="books">książki</label>
-                    </div>
-                    <div>
-                        <input type="checkbox" name={'different'} id={'different'} value={different}
-                               checked={different} onClick={handleInputs}/>
-                        <label htmlFor="different">inne</label>
-                    </div>
+                    <Step1 form={form}/>
                     <button onClick={(e) => handleSubmit(e)}>Dalej</button>
                 </form>
             </div>
@@ -79,7 +31,6 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
     return {
         submitStep: step => dispatch(submitStep(step)),
-        updateStep: step => dispatch(updateStep(step))
     }
 }
 
---
+++ src/components/Form/FormSection/Steps/Step1.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import {updateStep} from "../../../../actions";
+import {connect} from "react-redux";
+
+function Step1({form: {clothesToUse, clothesToTrash, toys, books, different}, updateStep}) {
+    const handleInputs = (event) => {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        updateStep({
+            [name]: value
+        })
+    };
+    return (
+        <>
+            <h2>Zaznacz co chcesz oddać:</h2>
+            <div>
+                <input type="checkbox" name={'clothesToUse'} id={'clothesToUse'} value={clothesToUse}
+                       checked={clothesToUse} onClick={handleInputs}/>
+                <label htmlFor="clothesToUse">ubrania, które nadają się do ponownego użycia</label>
+            </div>
+            <div>
+                <input type="checkbox" name={'clothesToTrash'} id={'clothesToTrash'} value={clothesToTrash}
+                       checked={clothesToTrash} onClick={handleInputs}/>
+                <label htmlFor="clothesToTrash">ubrania, do wyrzucenia</label>
+            </div>
+            <div>
+                <input type="checkbox" name={'toys'} id={'toys'} value={toys} checked={toys}
+                       onClick={handleInputs}/>
+                <label htmlFor="toys">zabawki</label>
+            </div>
+            <div>
+                <input type="checkbox" name={'books'} id={'books'} value={books} checked={books}
+                       onClick={handleInputs}/>
+                <label htmlFor="books">książki</label>
+            </div>
+            <div>
+                <input type="checkbox" name={'different'} id={'different'} value={different}
+                       checked={different} onClick={handleInputs}/>
+                <label htmlFor="different">inne</label>
+            </div>
+        </>
+    );
+}
+const mapDispatchToProps = dispatch => {
+    return {
+        updateStep: step => dispatch(updateStep(step))
+    }
+}
+
+export default connect(null, mapDispatchToProps)(Step1);

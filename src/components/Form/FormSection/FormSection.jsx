c5cce8e761cc00faef492400e70d701892b711c8
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-09-28 14:45:43 +0200

    add basic change step logic

--- src/components/Form/FormSection/FormSection.jsx
+++ src/components/Form/FormSection/FormSection.jsx
@@ -3,12 +3,23 @@ import style from './FormSection.module.scss';
 import {connect} from "react-redux";
 import {submitStep} from "../../../actions";
 import Step1 from "./Steps/Step1";
+import Step2 from "./Steps/Step2";
 
 function FormSection({form, submitStep}) {
     const [currentStep, setCurrentStep] = useState(1)
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        const currentPage = e.target.id;
+
+
+        setCurrentStep(prevState => {
+            if (currentPage === 'nextPage'){
+                return prevState === 4 ? 4 : prevState + 1;
+            }else {
+                return prevState === 1 ? 1 : prevState - 1;
+            }
+        })
     }
 
     return (
@@ -16,8 +27,13 @@ function FormSection({form, submitStep}) {
             <div className={style.FormSection__container}>
                 <h4>Krok {currentStep}/4</h4>
                 <form action="#">
-                    <Step1 form={form}/>
-                    <button onClick={(e) => handleSubmit(e)}>Dalej</button>
+                    {/*<Step1 form={form}/>*/}
+                    <Step2 form={form}/>
+
+                    <div className={style.buttons}>
+                        <button id={'prevPage'} onClick={(e) => handleSubmit(e)}>Wstecz</button>
+                        <button id={'nextPage'} onClick={(e) => handleSubmit(e)}>Dalej</button>
+                    </div>
                 </form>
             </div>
         </section>

eb691797aa94391a7336108e965d8eee9a1008f2
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-09-28 14:32:07 +0200

    create Step2

---
+++ src/components/Form/FormSection/Steps/Step2.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import style from './Steps.module.scss'
+import {updateStep} from "../../../../actions";
+import {connect} from "react-redux";
+
+function Step2({form: { trashBagsAmount }, updateStep}) {
+    const handleSelect = (event) => {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        updateStep({
+            [name]: value
+        })
+    };
+    console.log(trashBagsAmount);
+    return (
+        <div className={style.steps}>
+            <h2>Podaj liczbę 60l worków, w które spakowałeś/aś rzeczy:</h2>
+            <div className={style.step2}>
+                <p>Liczba worków 60L:</p>
+                <select name="trashBagsAmount" id="trashBagsAmount" onChange={e => handleSelect(e)} value={trashBagsAmount}>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="more">Więcej</option>
+                </select>
+            </div>
+        </div>
+    );
+}
+const mapDispatchToProps = dispatch => {
+    return {
+        updateStep: step => dispatch(updateStep(step))
+    }
+}
+
+export default connect(null, mapDispatchToProps)(Step2);

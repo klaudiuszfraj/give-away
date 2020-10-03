26f976bfcb136b4f1d57e128e9a128852c7ca5d0
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-10-03 10:25:07 +0200

    add dispaly date

--- src/components/Form/FormSection/Steps/Summary.jsx
+++ src/components/Form/FormSection/Steps/Summary.jsx
@@ -1,8 +1,51 @@
-import React, {useState} from 'react';
+import React from 'react';
 import style from './Steps.module.scss'
 
 
-function Summary(form) {
+function Summary({
+                     form: {
+                         clothesToUse,
+                         clothesToTrash,
+                         toys,
+                         books,
+                         different,
+                         trashBagsAmount,
+                         localization,
+                         kids,
+                         singleMothers,
+                         homeless,
+                         disabled,
+                         elderly,
+                         userOrganization,
+                         street,
+                         city,
+                         postcode,
+                         mobile,
+                         date,
+                         messageForCourier
+                     }
+                 }) {
+    //todo:: repalce in redux
+    const renderItems = {
+        clothesToUse,
+        clothesToTrash,
+        toys,
+        books,
+        different,
+    };
+
+    const dateToString = {
+        day: date.getDate(),
+        month: date.getMonth().length < 1 ? 0 + date.getMonth() : date.getMonth(),
+        year: date.getFullYear()
+    }
+    console.log(dateToString)
+
+    const myDateString = ('0' + date.getDate()).slice(-2) + '/'
+        + ('0' + (date.getMonth()+1)).slice(-2) + '/'
+        + date.getFullYear();
+    console.log(myDateString);
+
     return (
         <div className={style.summary}>
             <h2>Podsumowanie Twojej darowizny</h2>
@@ -10,41 +53,47 @@ function Summary(form) {
                 <div className={style.giveaway}>
                     <p>Oddajesz:</p>
                     <div>
-                        <p>4dfadaasd sfd</p>
-                        <p>dla lokalizacji:</p>
+                        {/*todo:: render items*/}
+                        <p>{trashBagsAmount} worki, </p>
+                        <p className={style.localization}>dla lokalizacji: {localization}</p>
                     </div>
                 </div>
-                <div className={style.adress}>
-                    <p>Adres odbioru:</p>
+                <div className={style.adress__sec}>
                     <div>
-                        <p>Ulica</p>
-                        <p>nnnnn</p>
+                        <p className={style.title}>Adres odbioru:</p>
+                        <div>
+                            <p>Ulica</p>
+                            <p>{street}</p>
+                        </div>
+                        <div>
+                            <p>Miasto</p>
+                            <p>{city}</p>
+                        </div>
+                        <div>
+                            <p>Kod pocztowy</p>
+                            <p>{postcode}</p>
+                        </div>
+                        <div>
+                            <p>Nume telefonu</p>
+                            <p>{mobile}</p>
+                        </div>
                     </div>
                     <div>
-                        <p>Miasto</p>
-                        <p>nnn</p>
+                        <p className={style.title}>Termin odbioru:</p>
+                        <div>
+                            <p>Data</p>
+                            <p>{`${myDateString}r`}</p>
+                        </div>
+                        <div>
+                            <p>Godzina</p>
+                            <p>{`${date.getHours()}:${date.getMinutes()}`}</p>
+                        </div>
+                        <div>
+                            {/*todo:: space-between*/}
+                            <p>Uwagi dla kuriera</p>
+                            <p>{messageForCourier}</p>
+                        </div>
                     </div>
-                    <div>
-                        <p>Kod pocztowy</p>
-                        <p>nnn</p>
-                    </div>
-                </div>
-                <div>
-                    <p>Nume telefonu</p>
-                    <p>nnn</p>
-                </div>
-                <p>Termin odbioru</p>
-                <div>
-                    <p>Data</p>
-                    <p>dddd</p>
-                </div>
-                <div>
-                    <p>Godzina</p>
-                    <p>ggggggg</p>
-                </div>
-                <div>
-                    <p>Uwagi dla kuriera</p>
-                    <p>uuuuu</p>
                 </div>
             </div>
         </div>

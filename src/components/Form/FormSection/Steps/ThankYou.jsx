a3d65ef3a2d81aa83124822ab37c042225601ce6
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-10-04 14:48:55 +0200

    add content

--- src/components/Form/FormSection/Steps/ThankYou.jsx
+++ src/components/Form/FormSection/Steps/ThankYou.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import style from './Steps.module.scss'
+import {ReactComponent as Decoration} from "../../../../assets/Decoration.svg";
 
 function ThankYou() {
     return (
-        <div>
-
+        <div className={style.ThankYou}>
+            <p>Dziękujemy za przesłanie formularza</p>
+            <p>Na maila prześlemy wszelkie</p>
+            <p>informacje o odbiorze.</p>
+            <Decoration/>
         </div>
     );
 }

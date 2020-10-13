344f7481e8229b4be10da25458d7bd24f160caaa
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-10-13 18:01:06 +0200

    chcnage onClick to onChange

--- src/components/Form/FormSection/Steps/Step1.jsx
+++ src/components/Form/FormSection/Steps/Step1.jsx
@@ -23,27 +23,27 @@ function Step1({form: {clothesToUse, clothesToTrash, toys, books, different}, up
             <h2>Zaznacz co chcesz oddać:</h2>
             <div>
                 <input type="checkbox" name={'clothesToUse'} id={'clothesToUse'} value={clothesToUse}
-                       checked={clothesToUse} onClick={handleInputs}/>
+                       checked={clothesToUse} onChange={handleInputs}/>
                 <label htmlFor="clothesToUse">ubrania, które nadają się do ponownego użycia</label>
             </div>
             <div>
                 <input type="checkbox" name={'clothesToTrash'} id={'clothesToTrash'} value={clothesToTrash}
-                       checked={clothesToTrash} onClick={handleInputs}/>
+                       checked={clothesToTrash} onChange={handleInputs}/>
                 <label htmlFor="clothesToTrash">ubrania, do wyrzucenia</label>
             </div>
             <div>
                 <input type="checkbox" name={'toys'} id={'toys'} value={toys} checked={toys}
-                       onClick={handleInputs}/>
+                       onChange={handleInputs}/>
                 <label htmlFor="toys">zabawki</label>
             </div>
             <div>
                 <input type="checkbox" name={'books'} id={'books'} value={books} checked={books}
-                       onClick={handleInputs}/>
+                       onChange={handleInputs}/>
                 <label htmlFor="books">książki</label>
             </div>
             <div>
                 <input type="checkbox" name={'different'} id={'different'} value={different}
-                       checked={different} onClick={handleInputs}/>
+                       checked={different} onChange={handleInputs}/>
                 <label htmlFor="different">inne</label>
             </div>
         </div>

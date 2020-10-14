bb34bce8b87f2782dbcd19d6daf2dafeedccb299
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-10-14 17:53:09 +0200

    fix issue #5

--- src/components/Form/FormSection/Steps/Step1.jsx
+++ src/components/Form/FormSection/Steps/Step1.jsx
@@ -22,28 +22,28 @@ function Step1({form: {clothesToUse, clothesToTrash, toys, books, different}, up
         <div className={classNames(style.steps)}>
             <h2>Zaznacz co chcesz oddać:</h2>
             <div>
-                <input type="checkbox" name={'clothesToUse'} id={'clothesToUse'} value={clothesToUse}
-                       checked={clothesToUse} onChange={handleInputs}/>
+                <input type="checkbox" name={'clothesToUse'} id={'clothesToUse'} value={clothesToUse.isChecked}
+                       checked={clothesToUse.isChecked} onChange={handleInputs}/>
                 <label htmlFor="clothesToUse">ubrania, które nadają się do ponownego użycia</label>
             </div>
             <div>
-                <input type="checkbox" name={'clothesToTrash'} id={'clothesToTrash'} value={clothesToTrash}
-                       checked={clothesToTrash} onChange={handleInputs}/>
+                <input type="checkbox" name={'clothesToTrash'} id={'clothesToTrash'} value={clothesToTrash.isChecked}
+                       checked={clothesToTrash.isChecked} onChange={handleInputs}/>
                 <label htmlFor="clothesToTrash">ubrania, do wyrzucenia</label>
             </div>
             <div>
-                <input type="checkbox" name={'toys'} id={'toys'} value={toys} checked={toys}
+                <input type="checkbox" name={'toys'} id={'toys'} value={toys.isChecked} checked={toys.isChecked}
                        onChange={handleInputs}/>
                 <label htmlFor="toys">zabawki</label>
             </div>
             <div>
-                <input type="checkbox" name={'books'} id={'books'} value={books} checked={books}
+                <input type="checkbox" name={'books'} id={'books'} value={books.isChecked} checked={books.isChecked}
                        onChange={handleInputs}/>
                 <label htmlFor="books">książki</label>
             </div>
             <div>
-                <input type="checkbox" name={'different'} id={'different'} value={different}
-                       checked={different} onChange={handleInputs}/>
+                <input type="checkbox" name={'different'} id={'different'} value={different.isChecked}
+                       checked={different.isChecked} onChange={handleInputs}/>
                 <label htmlFor="different">inne</label>
             </div>
         </div>

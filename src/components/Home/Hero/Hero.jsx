1308116b83b324121e7f820343f5a8623b29f7aa
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-10-17 07:53:41 +0200

    add start id

--- src/components/Home/Hero/Hero.jsx
+++ src/components/Home/Hero/Hero.jsx
@@ -5,7 +5,7 @@ import { ReactComponent as Decoration } from '../../../assets/Decoration.svg';
 
 function Hero() {
     return (
-        <section className={style.hero}>
+        <section className={style.hero} id={'start'}>
             <div className={style.hero__img}/>
             <div className={style.hero__info}>
             <h1>Zacznij pomagać!<br/>Oddaj niechciane rzeczy w zaufane ręce</h1>

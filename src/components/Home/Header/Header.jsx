1532577bfdd91e5c9662e7ec14a13ecb429ead55
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-10-17 15:15:19 +0200

    add offset to scroll

--- src/components/Home/Header/Header.jsx
+++ src/components/Home/Header/Header.jsx
@@ -9,7 +9,6 @@ import Hamburger from "./Hambuger/Hamburger";
 
 //todo:: links to map
 //todo:: reusable btn
-//todo:: add ofset to scroll
 function Header({auth: {uid, email}, profile: {initials, userColor}}) {
     const [isOpen, setIsOpen] = useState(false);
 
@@ -37,7 +36,7 @@ function Header({auth: {uid, email}, profile: {initials, userColor}}) {
                 <>
                     <nav className={classNames({'open': isOpen})}>
                         <ScrollLink to={'start'} smooth={true} duration={1000} onClick={()=>setIsOpen(false)}>Start</ScrollLink>
-                        <ScrollLink to={'FourSteps'} smooth={true} duration={1000}  onClick={()=>setIsOpen(false)}>O co chodzi?</ScrollLink>
+                        <ScrollLink to={'FourSteps'} smooth={true} duration={1000} offset={-100} onClick={()=>setIsOpen(false)}>O co chodzi?</ScrollLink>
                         <ScrollLink to={'aboutUs'} smooth={true} duration={1000}  onClick={()=>setIsOpen(false)}>O nas</ScrollLink>
                         <ScrollLink to={'foundation'} spy={true} smooth={true} duration={1000}  onClick={()=>setIsOpen(false)}>Fundcja i organizacje</ScrollLink>
                         <ScrollLink to={'contact'} smooth={true} duration={1000}  onClick={()=>setIsOpen(false)}>Kontakt</ScrollLink>
